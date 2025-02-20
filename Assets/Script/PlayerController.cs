1feaba0ae9e5910b32f20df605a002fa0cac1076
mushkinV <165308839+mushkinV@users.noreply.github.com>
2025-02-20 12:10:28 +0300

    Player_Functional

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+
+    private Rigidbody2D rb;
+
+    private bool facingRight = true;
+
+    private bool isGrounded;
+    public Transform feetPos;
+    public float checkRadius;
+    public LayerMask wnatIsGround;
+
+    private Animator anim;
+
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        moveInput = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new Vector2(moveInput * speed, rb.linearVelocity.y);
+        if (facingRight == false && moveInput > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && moveInput < 0)
+        {
+            Flip();
+        }
+        
+    }
+
+    private void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, wnatIsGround);
+
+        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.linearVelocity = Vector2.up * jumpForce;
+            anim.SetTrigger("takeOF");
+
+        }
+        if (isGrounded == true)
+        {
+            anim.SetBool("isJumping", false);
+        }
+        else
+        {
+            anim.SetBool("isJumping", true);
+        }
+
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+
+       
+    }
+}
